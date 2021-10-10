ef85845f1295e8305d149b5a5dbff18dfff34173
徐剑 <adamtcxj@gmail.com>
2021-10-10 20:45:40 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Sort/Select_sort/Select_sort.py

---
+++ Sort/Merge_sort/merge_sort.py
@@ -0,0 +1,56 @@
+#时间复杂度：O(nlogn)
+#空间复杂度：O(n)
+
+def merge(li, low, mid, high):
+    i = low
+    j = mid + 1
+    ltmp = []
+    while i <= mid and j <= high:  # 只要左右两边都有数
+        if li[i] < li[j]:
+            ltmp.append(li[i])
+            i += 1
+        else:
+            ltmp.append(li[j])
+            j += 1
+    # while执行完，肯定有一部分没救了
+    while i <= mid:
+        ltmp.append(li[i])
+        i += 1
+    while j <= high:
+        ltmp.append(li[j])
+        j += 1
+    li[low:high + 1] = ltmp
+
+li = [2, 4, 5, 7, 1, 3, 6, 8]
+merge(li, 0, 3, 7)
+print(li)
+
+def merge_sort(li,low,high):
+    if low<high:  #至少有两个元素，递归
+        mid=(low+high)//2
+        merge_sort(li,low,mid)
+        merge_sort(li,mid+1,high)
+        print(li[low:high+1])
+        #merge(li,low,mid,high)
+
+li=list(range(1000))
+import random
+random.shuffle(li)
+print(li)
+merge_sort(li,0,len(li)-1)
+print(li)
+
+
+# def merge_sort_test(li,low,high):
+#     if low<high:
+#         mid=(low+high)//2
+#         merge_sort_test(li,low,mid)
+#         merge_sort_test(li,mid+1,high)
+#         print(li[low:high+1])
+#
+# li=list(range(16))
+# import random
+# random.shuffle(li)
+# print(li)
+# merge_sort_test(li,0,len(li)-1)
+# print(li)
